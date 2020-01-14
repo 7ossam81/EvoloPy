31485126e1764567ad922c3e877811ceaf247fe7
Raneem Qaddoura <raneem.qaddoura@gmail.com>
2020-01-14 21:16:33 +0200

    Update DE.py

--- DE.py
+++ DE.py
@@ -1,42 +1,37 @@
 import random
 import numpy
-import math
-from colorama import Fore, Back, Style
 import time
-import collections
+from solution import solution
 
-class solution:
-    def __init__(self):
-        self.leader_fitness = 0
-        self.leader_solution=[]
-        self.func_evals = 0
 
 # Differential Evolution (DE)
 # mutation factor = [0.5, 2]
 # crossover_ratio = [0,1]
-def DE(population_size, individual_size,
-        lower_bound, upper_bound, fitness_func,iters_count,
-       mutation_factor=0.5, crossover_ratio=0.7,
-        stopping_func=None, verbose=False):
+def DE(objf,lb,ub,dim,PopSize,iters):
+    
+    mutation_factor=0.5
+    crossover_ratio=0.7
+    stopping_func=None
 
-    # convert lower_bound, upper_bound to array
-    if not isinstance(lower_bound, list):
-        lower_bound = [lower_bound for _ in range(individual_size)]
-        upper_bound = [upper_bound for _ in range(individual_size)]
+    # convert lb, ub to array
+    if not isinstance(lb, list):
+        lb = [lb for _ in range(dim)]
+        ub = [ub for _ in range(dim)]
 
     # solution
-    result = solution()
-    result.leader_fitness = float("inf")
+    s = solution()
+    
+    s.best = float("inf")
 
     # initialize population
     population = []
 
-    population_fitness = numpy.array([float("inf") for _ in range(population_size)])
+    population_fitness = numpy.array([float("inf") for _ in range(PopSize)])
 
-    for s in range(population_size):
+    for p in range(PopSize):
         sol = []
-        for d in range(individual_size):
-            d_val = random.uniform(lower_bound[d], upper_bound[d])
+        for d in range(dim):
+            d_val = random.uniform(lb[d], ub[d])
             sol.append(d_val)
 
         population.append(sol)
@@ -44,33 +39,39 @@ def DE(population_size, individual_size,
     population = numpy.array(population)
 
     # calculate fitness for all the population
-    for i in range(population_size):
-        fitness = fitness_func(population[i, :])
-        population_fitness[s] = fitness
-        result.func_evals += 1
+    for i in range(PopSize):
+        fitness = objf(population[i, :])
+        population_fitness[p] = fitness
+        #s.func_evals += 1
 
         # is leader ?
-        if fitness < result.leader_fitness:
-            result.leader_fitness = fitness
-            result.leader_solution = population[i, :]
+        if fitness < s.best:
+            s.best = fitness
+            s.leader_solution = population[i, :]
 
+    convergence_curve=numpy.zeros(iters)
     # start work
+    print("DE is optimizing  \""+objf.__name__+"\"")    
+    
+    timerStart=time.time() 
+    s.startTime=time.strftime("%Y-%m-%d-%H-%M-%S")
+    
     t = 0
-    while t < iters_count:
+    while t < iters:
         # should i stop
-        if stopping_func is not None and stopping_func(result.leader_fitness, result.leader_solution, t):
+        if stopping_func is not None and stopping_func(s.best, s.leader_solution, t):
             break
 
         # loop through population
-        for i in range(population_size):
+        for i in range(PopSize):
             # 1. Mutation
 
             # select 3 random solution except current solution
-            ids_except_current = [_ for _ in  range(population_size) if _ != i]
+            ids_except_current = [_ for _ in  range(PopSize) if _ != i]
             id_1, id_2, id_3 = random.sample(ids_except_current, 3)
 
             mutant_sol = []
-            for d in range(individual_size):
+            for d in range(dim):
                 d_val = population[id_1, d] + mutation_factor * (population[id_2, d] - population[id_3, d])
 
                 # 2. Recombination
@@ -84,11 +85,11 @@ def DE(population_size, individual_size,
             # 3. Replacement / Evaluation
 
             # clip new solution (mutant)
-            mutant_sol = numpy.clip(mutant_sol, lower_bound, upper_bound)
+            mutant_sol = numpy.clip(mutant_sol, lb, ub)
 
             # calc fitness
-            mutant_fitness = fitness_func(mutant_sol)
-            result.func_evals += 1
+            mutant_fitness = objf(mutant_sol)
+            #s.func_evals += 1
 
             # replace if mutant_fitness is better
             if mutant_fitness < population_fitness[i]:
@@ -96,17 +97,23 @@ def DE(population_size, individual_size,
                 population_fitness[i] = mutant_fitness
 
                 # update leader
-                if mutant_fitness < result.leader_fitness:
-                    result.leader_fitness = mutant_fitness
-                    result.leader_solution = mutant_sol
+                if mutant_fitness < s.best:
+                    s.best = mutant_fitness
+                    s.leader_solution = mutant_sol
 
-        # log
-        if t % 10 == 2 and verbose == True:
-            txt = "Generation=%s , Best Fitness=%s"
-            print(txt % (str(t + 1), result.leader_fitness))
+        convergence_curve[t]=s.best
+        if (t%1==0):
+               print(['At iteration '+ str(t+1)+ ' the best fitness is '+ str(s.best)]);
 
         # increase iterations
         t = t + 1
+        
+        timerEnd=time.time()  
+        s.endTime=time.strftime("%Y-%m-%d-%H-%M-%S")
+        s.executionTime=timerEnd-timerStart
+        s.convergence=convergence_curve
+        s.optimizer="DE"
+        s.objfname=objf.__name__
 
     # return solution
-    return result
+    return s
