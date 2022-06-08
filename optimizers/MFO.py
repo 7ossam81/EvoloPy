7cadcd90b92fdfaa3449b5909e0cd0d5d907d594
Raneem Qaddoura <raneem.qaddoura@gmail.com>
2022-06-08 13:21:50 +0300

    removed static N from MFO

--- optimizers/MFO.py
+++ optimizers/MFO.py
@@ -18,7 +18,7 @@ def MFO(objf, lb, ub, dim, N, Max_iteration):
     # lb=-100
     # ub=100
     # dim=30
-    N = 50  # Number of search agents
+    #N = 50  # Number of search agents
     if not isinstance(lb, list):
         lb = [lb] * dim
     if not isinstance(ub, list):
